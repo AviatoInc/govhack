b1ee6a06efbe3087e9698d62b3a9455ed687124d
2116025_Maharnav Deka <dekamaharnav200@gmail.com>
2023-07-05 19:05:49 +0530

    added department

--- app/department/layout.tsx
+++ app/department/layout.tsx
@@ -12,24 +12,13 @@ export const metadata: Metadata = {
 const sidebarNavItems = [
   {
     title: "Profile",
-    href: "/examples/forms",
+    href: "/department",
   },
   {
     title: "Account",
-    href: "/examples/forms/account",
+    href: "/department/account",
   },
-  {
-    title: "Appearance",
-    href: "/examples/forms/appearance",
-  },
-  {
-    title: "Notifications",
-    href: "/examples/forms/notifications",
-  },
-  {
-    title: "Display",
-    href: "/examples/forms/display",
-  },
+ 
 ]
 
 interface SettingsLayoutProps {
