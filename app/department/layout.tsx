8061c8ca205efa9215b6c519245c367c8e8fa7d6
2116025_Maharnav Deka <dekamaharnav200@gmail.com>
2023-07-05 20:35:02 +0530

    department template

--- app/department/layout.tsx
+++ app/department/layout.tsx
@@ -46,9 +46,9 @@ export default function SettingsLayout({ children }: SettingsLayoutProps) {
       </div>
       <div className="hidden space-y-6 p-10 pb-16 md:block">
         <div className="space-y-0.5">
-          <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
+          <h2 className="text-2xl font-bold tracking-tight">Department</h2>
           <p className="text-muted-foreground">
-            Manage your account settings and set e-mail preferences.
+            Manage your Schemes and Check your Data
           </p>
         </div>
         <Separator className="my-6" />
